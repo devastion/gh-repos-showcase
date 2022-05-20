aacc8f6e6d05b1cf21e4ed656a7df361d5b56980
Dimitar Banev <banev_dimitar@mail.com>
2022-05-21 01:35:58 +0300

    fix: moved load more into another branch

--- src/components/RepoCard/RepoCard.tsx
+++ src/components/RepoCard/RepoCard.tsx
@@ -16,7 +16,6 @@ export interface Repo {
 
 export default function RepoCard() {
   const [repos, setRepos] = React.useState<Repo[] | undefined>([]);
-  const [shown, setShown] = React.useState<number>(6);
   const options = React.useContext(Options);
   const {
     username,
@@ -49,9 +48,8 @@ export default function RepoCard() {
   const getLangColor = (lang: string) =>
     languagesColors && languagesColors[lang];
 
-  const renderRepos = repos
-    ?.slice(0, shown)
-    .map(({ id, name, description, language, homepage, html_url }) => {
+  const renderRepos = repos?.map(
+    ({ id, name, description, language, homepage, html_url }) => {
       const color = getLangColor(language);
 
       return (
@@ -81,16 +79,8 @@ export default function RepoCard() {
           </RepoCardContainer.ButtonsWrapper>
         </RepoCardContainer.RepoWrapper>
       );
-    });
-
-  const handleMore = () => setShown(shown + 3);
-
-  return (
-    <>
-      {renderRepos}
-      <button type="button" onClick={handleMore}>
-        load more
-      </button>
-    </>
+    }
   );
+
+  return <>{renderRepos}</>;
 }
